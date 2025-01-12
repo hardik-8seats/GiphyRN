a11947638fd1c05181190463883f1ea2b6f2d70c
Hardik Thakkar <hardik.thakkar@8seats.com>
2025-01-12 12:10:22 +1100

    ENV file added with GIPHY API KEY

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const apiKey = process.env.EXPO_PUBLIC_GIPHY_API_KEY;
+
   return (
     <View
       style={{
@@ -9,7 +11,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>API KEY: {apiKey}</Text>
     </View>
   );
 }
